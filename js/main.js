74ee50d141063e6da8fb665df4b372538856b6db
Busna27 <lucabusnelli2001@gmail.com>
2022-03-17 17:03:28 +0100

    Bonus 1 - done

--- js/main.js
+++ js/main.js
@@ -55,89 +55,12 @@ const app = new Vue (
                     console.warn('activeElement = imageArray.length - 1');
                 }
                 console.log('Hai cliccato next!');
+            },
+
+            clickThumb : function(imgIndex)
+            {
+                this.activeElement = imgIndex;
             }
         }
     }
 )
-
-
-
-
-
-// document.getElementById('my-before-carousel').innerHTML = `<h1>Beautifull Carousel</h1>`
-// document.getElementById('my-after-carousel').innerHTML = `<p>Designed by Busna</p>`
-
-
-// const imageContainer = document.querySelector('.my-carousel-images');
-// const thumbnailContainer = document.querySelector('.my-thumbnails');
-
-
-// for (let i = 0; i < imageArray.length; i++) {
-//     imageContainer.innerHTML += `<img class="img-fluid my-carousel-item" src="${imageArray[i].src}" alt="${imageArray[i].name}">`
-
-//     thumbnailContainer.innerHTML += `<img class="my-miniature-item" src="${imageArray[i].src}" alt="${imageArray[i].name}">`
-// }
-
-
-// const carouselElements = document.getElementsByClassName('my-carousel-item');
-// const miniatureElements = document.getElementsByClassName('my-miniature-item');
-// console.log(miniatureElements);
-
-
-// carouselElements[0].classList.add('my-active');
-// miniatureElements[0].classList.add('my-opacity-min');
-
-
-// const prevBtn = document.querySelector('.my-previous');
-// const nextBtn = document.querySelector('.my-next');
-// let activeElement = 0;
-// prevBtn.addEventListener('click', function() {
-//     // Remove active from the current items
-//     carouselElements[activeElement].classList.remove('my-active');
-//     miniatureElements[activeElement].classList.remove('my-opacity-min');
-
-//     activeElement--;
-
-//     // Loop if condition that return to end
-//     if(activeElement < 0) {
-//         activeElement = carouselElements.length - 1;
-//     }
-
-//     // Add active to the previous items
-//     carouselElements[activeElement].classList.add('my-active');
-//     miniatureElements[activeElement].classList.add('my-opacity-min');
-// });
-
-// nextBtn.addEventListener('click', function() {
-//     // Remove active from the current items
-//     carouselElements[activeElement].classList.remove('my-active');
-//     miniatureElements[activeElement].classList.remove('my-opacity-min');
-
-//     activeElement++;
-
-//     // Loop if condition that return to start
-//     if(activeElement > (carouselElements.length - 1)) {
-//         activeElement = 0;
-//     }
-
-//     // Add active to the next items
-//     carouselElements[activeElement].classList.add('my-active');
-//     miniatureElements[activeElement].classList.add('my-opacity-min');
-// });
-
-
-// setInterval(() => {
-//     carouselElements[activeElement].classList.remove('my-active');
-//     miniatureElements[activeElement].classList.remove('my-opacity-min');
-
-//     activeElement++;
-
-//     // Loop if condition that return to start
-//     if(activeElement > (carouselElements.length - 1)) {
-//         activeElement = 0;
-//     }
-
-//     // Add active to the next items
-//     carouselElements[activeElement].classList.add('my-active');
-//     miniatureElements[activeElement].classList.add('my-opacity-min');
-// }, 2500);
